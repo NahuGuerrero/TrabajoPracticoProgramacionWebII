60eabbb6c655cb0219b0c97d1b871ec98583750d
NahuGuerrero <164700786+NahuGuerrero@users.noreply.github.com>
2025-11-22 18:16:55 -0300

    Create MaestriaInsuficienteException.java

---
+++ src/ar/edu/unlam/pb2/SistemaDeGestionDeCriaturas/MaestriaInsuficienteException.java
@@ -0,0 +1,8 @@
+package ar.edu.unlam.pb2.SistemaDeGestionDeCriaturas;
+
+public class MaestriaInsuficienteException extends Exception {
+
+    public MaestriaInsuficienteException(String mensaje) {
+        super(mensaje);
+    }
+}
